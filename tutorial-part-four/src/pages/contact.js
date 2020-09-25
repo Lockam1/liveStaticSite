d7c9d683714eef1a3ea99c651fee6c501d5d3bcd
lockam1 <lockam1@student.op.ac.nz>
2020-09-25 13:21:14 +1200

    Added test site

---
+++ tutorial-part-four/src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Layout from "../components/layout"
+export default function Contact() {
+  return (
+    <Layout>
+        <h1>Contact Us</h1>
+        <div>
+            <form method="post" action="/">
+                <label>
+                    Name
+                    <input type="text" name="name" id="name" />
+                </label>
+                <label>
+                    Email
+                    <input type="email" name="email" id="email" />
+                </label>
+                <label>
+                    Subject
+                    <input type="text" name="subject" id="subject" />
+                </label>
+                <label>
+                    Message
+                    <textarea name="message" id="message" rows="5" />
+                </label>
+                <button type="submit">Send</button>
+                <input type="reset" value="Clear" />
+            </form>
+        </div>
+    </Layout>
+  )
+}
